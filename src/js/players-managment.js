f71e08e4971dba2cc350a81c97f6ae0e75cf63ba
Gabriela <gabi16.hernandez@gmail.com>
2022-08-25 16:51:50 -0600

    Cambios no funcionales para players

--- src/js/players-managment.js
+++ src/js/players-managment.js
@@ -1,18 +1,40 @@
-function getListOfPlayers() {
-  var requestOptions = {
-    method: "GET",
-  };
+const URL = "https://jsonplaceholder.typicode.com/todos";
+let deletePlayersbtn = [];
+getListOfPlayers();
+const tablePlayers = document.querySelector("#players tbody");
+const createPlayersbtn = document.querySelector("#createPlayer");
+const playerName = document.querySelector("#playerName");
+const playerType = document.querySelector("#playerName");
 
-  fetch("https://jsonplaceholder.typicode.com/todos", requestOptions)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
-    .catch((error) => console.log("error", error));
+deletePlayersbtn.forEach((btn) => (btn.onclick = () => console.log("Hi")));
+
+
+
+function getListOfPlayers() {
+  let requestResult = [];
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      requestResult = json
+
+        .map((element) => {
+          return `
+           <tr id="${element.id}">
+            <td>${element.title}</td>
+            <td><button class="delete-btn">Delete</button></td>
+          </tr>
+        `;
+        })
+        .join("");
+      tablePlayers.innerHTML = requestResult;
+      deletePlayersbtn = document.querySelectorAll(".delete-btn");
+    });
 }
 
-function deletPlayer(idPlayer) {
+function deletePlayer(idPlayer) {
   var formdata = new FormData();
-  formdata.append("id", idPlayer);
-  //formdata.append("idPlayer", idPlayer);
 
   var requestOptions = {
     method: "DELETE",
@@ -20,12 +42,21 @@ function deletPlayer(idPlayer) {
     redirect: "follow",
   };
 
-  fetch("https://jsonplaceholder.typicode.com/todos", requestOptions)
+  fetch(
+    "https://jsonplaceholder.typicode.com/posts/" + idPlayer,
+    requestOptions
+  )
     .then((response) => response.text())
     .then((result) => console.log(result))
     .catch((error) => console.log("error", error));
 }
 
+/*function bridgeDeletePlayer(e) {
+  console.log("Hola");
+  console.log(e.target);
+  //deletePlayer();
+}
+*/
 function createPlayer(newName, newType) {
   var formdata = new FormData();
   formdata.append("name", newName);
@@ -43,4 +74,12 @@ function createPlayer(newName, newType) {
       console.log(result)
     )
     .catch((error) => console.log("error", error));
+}
+
+createPlayersbtn.addEventListener("click",bridgecreatePlayer);
+
+function bridgecreatePlayer(e) {
+  console.log("Hola");
+  console.log(e.target.id);
+
 }
