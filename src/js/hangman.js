200794e573fbd84fdfee444dd36a2f42a4e8a7b4
Jesus Barrera <jesus.barrera@telusinternational.com>
2022-08-26 16:14:06 -0600

    Created show list of turn players funcionality

--- src/js/hangman.js
+++ src/js/hangman.js
@@ -1,30 +1,66 @@
 const players = document.querySelector('#players tbody');
-const URL_TODOS = 'https://jsonplaceholder.typicode.com/todos';
+const startGameBtn = document.querySelector('#startGame');
+const URL_TODOS = 'https://jsonplaceholder.typicode.com/todos/';
+const MAX_NUMBER_OF_PLAYERS = 2;
+let checkBoxes = [];
+let selectedPlayers = [];
 
 function showPlayers() {
-
-    var requestOptions = {
+    const requestOptions = {
         method: "GET",
-
     };
-
-    fetch(URL_TODOS,requestOptions)
+    fetch(URL_TODOS, requestOptions)
         .then((response) => response.json())
         .then((json) => {
-            console.log(json);
-            requestResult = json
+            return json
                 .map((element) => {
                     return `
-                <tr id="${element.id}">
-                <td>${element.title.slice(0, 10)}</td>
-                <td><input type="checkbox" value=${element.id} name="playerSelected"></td>
-                </tr>
-                `;
+                            <tr id="${element.id}">
+                                <td>${element.title.slice(0, 10)}</td>
+                                <td>
+                                    <input type="checkbox" value=${element.id} name="playerSelected">
+                                </td>
+                            </tr>
+                            `;
                 })
                 .slice(0, 10)
                 .join("");
-            players.innerHTML = requestResult;
-        });
+        })
+        .then(result => players.innerHTML = result)
+        .then(() => checkBoxes = document.querySelectorAll('[name=playerSelected]'))
+        .then(() => checkBoxes.forEach(checkBox => checkBox.addEventListener("click", checkCheckBoxes)));
 }
+
+function checkCheckBoxes() {
+    const numberOfPlayers = [...checkBoxes].filter(checkbox => checkbox.checked).length;
+    if (numberOfPlayers >= MAX_NUMBER_OF_PLAYERS) 
+        checkBoxes.forEach(checkbox => !checkbox.checked ? checkbox.disabled = true : "");
+    else     
+        checkBoxes.forEach(checkbox => checkbox.disabled = false);
+}        
+
+function startGameHandler() {
+    selectedPlayers = [...checkBoxes].filter(checkBox => checkBox.checked);
+    if (selectedPlayers.length !== MAX_NUMBER_OF_PLAYERS) return;
+    const [player1, player2] = [...selectedPlayers];
+    createNewTurn(player1.value, player2.value);
+}    
+
+function createNewTurn(player1, player2) {
+    const formdata = new FormData();
+    formdata.append("player1", player1);
+    formdata.append("player2", player2);
+
+    const requestOptions = {
+        method: "POST",
+        body: formdata,
+    };    
+
+    fetch(URL_TODOS, requestOptions)
+        .then(response => response.json())
+        .then(json => console.log(json));
+}        
+
+startGameBtn.addEventListener('click', startGameHandler);
 
 showPlayers();
