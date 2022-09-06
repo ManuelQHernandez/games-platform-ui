36efadffd2ab1a16f708e26dec4f122d197ede99
ManuelQHernandez <manuel.quinteros@telusinternational.com>
2022-09-06 04:12:43 -0600

    I added functionality on delete button and create-player form

--- src/js/create-player.js
+++ src/js/create-player.js
@@ -1,12 +1,14 @@
-const URL_TODOS = "https://jsonplaceholder.typicode.com/todos/";
+//const URL_TODOS = "https://jsonplaceholder.typicode.com/todos/";
+const URL_TODOS = "http://localhost:8080/api/players/";
 
-const playerName = document.querySelector("#playerName");
+
 const createPlayersbtn = document.querySelector("#createPlayer");
+
 
 function createPlayer(newName, newType) {
   var formdata = new FormData();
   formdata.append("name", newName);
-  formdata.append("type", newType);
+  formdata.append("typePlayer", newType);
 
   var requestOptions = {
     method: "POST",
@@ -14,17 +16,44 @@ function createPlayer(newName, newType) {
   };
 
   fetch(URL_TODOS, requestOptions)
-    .then((response) => response.text())
-    .then((result) =>
+    .then((response) => response.json())
+    .then((result) => {
+      
       //Here you will create the player
+      //playerName.innerText = result.newName;
       console.log(result)
-    )
+      alert('Player Added', 'success');
+
+    })
     .catch((error) => console.log("error", error));
 }
 
 createPlayersbtn.addEventListener("click", () => {
-  const typeChecked = document.querySelector(
-    "input[name=playerType]:checked"
-  ).value;
-  createPlayer(playerName, typeChecked);
+  const playerName = document.querySelector("#playerName")?.value;
+  const typeChecked = document.querySelector("input[name=playerType]:checked")?.value;
+  
+
+  // Validate
+  if(playerName == "" || !typeChecked)  {
+
+    alert('Please fill in all fields');
+
+  } else {
+    console.log(typeChecked)
+    console.log(playerName)
+    createPlayer(playerName, typeChecked);
+  }
+
+  
 });
+
+// Show Alerts
+function showAlert(message, className) {
+  const div =document.createElement("div");
+  div.className = `alert alert-${className}`;
+
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector(".container");
+  const main = document.querySelector(".main");
+  container.insertBefore(div, main);
+}
