216839b92f09f76111a0a9ce2598edce579e99ac
ManuelQHernandez <manuel.quinteros@telusinternational.com>
2022-09-06 04:19:11 -0600

    Create-player was fix

--- src/js/create-player.js
+++ src/js/create-player.js
@@ -47,13 +47,3 @@ createPlayersbtn.addEventListener("click", () => {
   
 });
 
-// Show Alerts
-function showAlert(message, className) {
-  const div =document.createElement("div");
-  div.className = `alert alert-${className}`;
-
-  div.appendChild(document.createTextNode(message));
-  const container = document.querySelector(".container");
-  const main = document.querySelector(".main");
-  container.insertBefore(div, main);
-}
